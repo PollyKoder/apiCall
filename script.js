deebf8878c8f2461ac1cbe78684a0c2b1a12d491
Polly <riviera.koder@gmail.com>
2023-11-09 12:54:25 +0100

    edit3

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-/* API link to use in function:  https://zenquotes.io/api/quotes/ */
+/* API link to use in function:  https://zenquotes.io/api/quotes/, this link only gave error and sked for a key despite the page stating that it would not need one */
+/*Trying this link instead: https://type.fit/api/quotes */
+
+
+//collects the div-cards class, so it can be manipulated
+const inspirationCard = document.querySelector(".inspoCard");
+
+//fetches data from the url and pushes/displays it browser
+function getQuote() {
+    fetch("https://type.fit/api/quotes")
+    .then((response) => response.json())
+    .then((data) => {
+        inspirationCard.innerHTML = "";
+        console.log(data);
+        const newQuote = document.createElement("p");
+        newQuote.innerText = data.array/text;
+        inspirationCard.appendChild(newQuote);
+    })
+    .catch((error) => console.log(error));
+}
