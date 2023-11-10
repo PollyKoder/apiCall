9db7ff946cbdabcc0a3a3dc7182249142a64b7c1
Polly <riviera.koder@gmail.com>
2023-11-10 13:30:17 +0100

    edit6

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 /* API link to use in function:  https://zenquotes.io/api/quotes/, this link only gave error and sked for a key despite the page stating that it would not need one */
 /*Trying this link instead: https://type.fit/api/quotes */
 
+/*After multiple problems with the other links followed by troubleshooting, I decided to test if the same issues occur if I tried with the pokemon api that was suggested in the assigment*/
+
 
 //collects the div-cards class, so it can be manipulated
-const inspirationCard = document.querySelector(".inspoCard");
+const inspirationCard = document.querySelector(".outputCard");
 
 //fetches data from the url and pushes/displays it browser
 function getQuote() {
-    fetch("https://type.fit/api/quotes")
+    fetch("https://pokeapi.co/api/v2/pokemon/ditto")
     .then((response) => response.json())
     .then((data) => {
         inspirationCard.innerHTML = "";
         console.log(data);
         const newQuote = document.createElement("p");
-        newQuote.innerText = data.text;
+        newQuote.innerText = data;
         inspirationCard.appendChild(newQuote);
     })
     .catch((error) => console.log(error));
