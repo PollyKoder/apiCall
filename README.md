e415d3eb5f0be1feda77c6ba3ea14396c1d6a86b
Polly <riviera.koder@gmail.com>
2023-11-10 14:01:53 +0100

    edit6

---
+++ README.md
@@ -0,0 +1,11 @@
+# NOTE
+
+The page seems to be ok now visually, after some troubleshooting, but the data from the API is still not being pushed to the page itself. It gives the information from the API in the console log, but only gives "undefined" in the divCard on the page itself. I've requested help from teacher and class mates, have not received reply from teacher and classmates who have tried to help cannot seem to find the error either.
+
+A class mate used chatGPT(sidenote, yes I have tried to use chatGPT, but it does not work and only gives error whenever I write something), and I have copy, pasted the code to test if it works. As noted above the code from chatGPT that is now commented out; it does work, but only gives one value from the API, when I would like it to collect values/quotes at random.
+
+Got another chatGPT code from  a classmate, this one does work as intended... Not satisfied with why I could not get my own code to work though.
+
+
+css note: I see that the button could be styled better, it does do it job, but there is no visible respnse when the button is clicked unlike previous pages I coded.
+Also I've spent so much time trying to figure out why the API would not play along that I don't remember why I made space for a banner/header on top of the page, as of now it only adds to aesthetics and nothing else.
--- script.js
+++ script.js
@@ -1,22 +1,79 @@
-/* API link to use in function:  https://zenquotes.io/api/quotes/, this link only gave error and sked for a key despite the page stating that it would not need one */
+/* API link to use in function:  https://zenquotes.io/api/quotes/, this link only gave error and asked for a key despite the page stating that it would not need one */
 /*Trying this link instead: https://type.fit/api/quotes */
 
-/*After multiple problems with the other links followed by troubleshooting, I decided to test if the same issues occur if I tried with the pokemon api that was suggested in the assigment*/
+/*After multiple problems with the other links followed by troubleshooting, 
+I decided to test if the same issues occur if I tried with the pokemon api that was 
+suggested in the assigment.
+Still just gives info in the console log and "undefined as output*/
 
 
 //collects the div-cards class, so it can be manipulated
 const inspirationCard = document.querySelector(".outputCard");
 
-//fetches data from the url and pushes/displays it browser
+//fetches data from the url and pushes/displays it browser(MY code!)
+//  function getQuote() {
+//      fetch("https://type.fit/api/quotes")
+//      .then((response) => response.json())
+//      .then((data) => {
+//          inspirationCard.innerHTML = "";
+//          console.log(data);
+//          const newQuote = document.createElement("p");
+//          newQuote.innerText = data;
+//          inspirationCard.appendChild(newQuote);
+//      })
+//      .catch((error) => console.log(error));
+// }
+
+
+/*Got a code from chatGPT that seems to work, tho I'm not clear on if I comprehend this code. Also it 
+only collects one of the values from the API*/
+// function getQuote() {
+//     fetch("https://type.fit/api/quotes")
+//     .then((response) => {
+//         if (!response.ok) {
+//             throw new Error("Network response was not ok");
+//         }
+//         return response.json();
+//     })
+//     .then((data) => {
+//         console.log(data);
+//         if (data.length > 0) {
+//             const newQuote = document.createElement("p");
+//             newQuote.innerText = data[0].text;
+//             inspirationCard.innerHTML = "";
+//             inspirationCard.appendChild(newQuote);
+//         } else {
+//             console.error("No quotes found in the API response");
+//         }
+//     })
+//     .catch((error) => console.error("Error fetching data:", error));
+// }
+
+//copy pasting another code from chatGPT to see if it runs correctly and if it does, I'll need to dissect it and figure out what makes it work
 function getQuote() {
-    fetch("https://pokeapi.co/api/v2/pokemon/ditto")
-    .then((response) => response.json())
-    .then((data) => {
-        inspirationCard.innerHTML = "";
-        console.log(data);
-        const newQuote = document.createElement("p");
-        newQuote.innerText = data;
-        inspirationCard.appendChild(newQuote);
+    fetch("https://type.fit/api/quotes")
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+        return response.json();
     })
-    .catch((error) => console.log(error));
+    .then((data) => {
+        console.log(data);
+        if (data.length > 0) {
+            const randomIndex = Math.floor(Math.random() * data.length);
+            const randomQuote = data[randomIndex].text;
+
+            const newQuote = document.createElement("p");
+            newQuote.innerText = randomQuote;
+
+            inspirationCard.innerHTML = "";
+            inspirationCard.appendChild(newQuote);
+        } else {
+            console.error("No quotes found in the API response");
+        }
+    })
+    .catch((error) => console.error("Error fetching data:", error));
 }
+
+//The last chatGPT code does work, but it has code in it that is foreign to me...
